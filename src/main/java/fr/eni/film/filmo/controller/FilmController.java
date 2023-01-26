685b494aef35c7835c9d4e79478512ed1f9e416f
Meysson17 <102948238+Meysson17@users.noreply.github.com>
2023-01-26 11:29:32 +0100

    Update FilmController.java

--- src/main/java/fr/eni/film/filmo/controller/FilmController.java
+++ src/main/java/fr/eni/film/filmo/controller/FilmController.java
@@ -2,16 +2,29 @@ package fr.eni.film.filmo.controller;
 
 import fr.eni.film.filmo.bll.FilmService;
 import fr.eni.film.filmo.bo.FilmBo;
+import org.springframework.ui.Model;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
 
+@Controller
+@SessionAttributes("film")
 public class FilmController {
 
     private FilmBo film() {return new FilmBo();}
-
     @Autowired
     private FilmService service;
 
-    public FilmController(FilmService service){this.service = service;}
+    public FilmController(FilmService service){
+        this.service = service;
+    }
 
-    
+    @GetMapping({"/", "/home"})
+    public String accueil() {
+        return "home";
+    }
+
+
+
 }
